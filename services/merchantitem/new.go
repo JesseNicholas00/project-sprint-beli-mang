aefc8f2203c33b682843ed4cc728bc077c5d49f0
Jesse Nicholas <jessenicholas.t@gmail.com>
2024-05-24 23:24:07 +0700

    wrap create item with transaction

--- services/merchantitem/new.go
+++ services/merchantitem/new.go
@@ -3,16 +3,23 @@ package merchantitem
 import (
 	merchant "github.com/JesseNicholas00/BeliMang/repos/merchant"
 	merchantitem "github.com/JesseNicholas00/BeliMang/repos/merchantitem"
+	"github.com/JesseNicholas00/BeliMang/utils/ctxrizz"
 )
 
 type merchantItemServiceImpl struct {
-	mRepo  merchant.MerchantRepository
-	miRepo merchantitem.MerchantItemRepository
+	mRepo    merchant.MerchantRepository
+	miRepo   merchantitem.MerchantItemRepository
+	dbRizzer ctxrizz.DbContextRizzer
 }
 
 func NewMerchantItemService(
 	mRepo merchant.MerchantRepository,
 	miRepo merchantitem.MerchantItemRepository,
+	dbRizzer ctxrizz.DbContextRizzer,
 ) MerchantItemService {
-	return &merchantItemServiceImpl{miRepo: miRepo, mRepo: mRepo}
+	return &merchantItemServiceImpl{
+		miRepo:   miRepo,
+		mRepo:    mRepo,
+		dbRizzer: dbRizzer,
+	}
 }
