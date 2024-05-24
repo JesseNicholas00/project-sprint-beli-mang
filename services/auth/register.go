6afbb68a7b704c3243be3886ac324ee61a658458
Jesse Nicholas <jessenicholas.t@gmail.com>
2024-05-25 00:50:10 +0700

    adding transaction to authentication flow

--- services/auth/register.go
+++ services/auth/register.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/JesseNicholas00/BeliMang/repos/auth"
 	"github.com/JesseNicholas00/BeliMang/utils/errorutil"
+	"github.com/JesseNicholas00/BeliMang/utils/transaction"
 	"github.com/google/uuid"
 	"golang.org/x/crypto/bcrypt"
 )
@@ -19,56 +20,62 @@ func (svc *authServiceImpl) RegisterUser(
 		return err
 	}
 
-	_, err := svc.repo.FindUserByUsername(ctx, req.Username)
-
-	if err == nil {
-		// duplicate username
-		return ErrUsernameAlreadyRegistered
-	}
-
-	if !errors.Is(err, auth.ErrUsernameNotFound) {
-		// unexpected kind of error
-		return errorutil.AddCurrentContext(err)
-	}
-
-	_, err = svc.repo.FindUserByEmailAndIsAdmin(ctx, req.Email, req.Role == "admin")
-
-	if err == nil {
-		return ErrEmailAlreadyRegistered
-	}
-
-	if !errors.Is(err, auth.ErrEmailAndIsAdminNotFound) {
-		// unexpected kind of error
-		return errorutil.AddCurrentContext(err)
-	}
-
-	cryptedPw, err := bcrypt.GenerateFromPassword(
-		[]byte(req.Password),
-		svc.bcryptCost,
-	)
+	ctx, sess, err := svc.dbRizzer.GetOrAppendTx(ctx)
 	if err != nil {
 		return errorutil.AddCurrentContext(err)
 	}
 
-	repoRes, err := svc.repo.CreateUser(ctx, auth.User{
-		Id:       uuid.New().String(),
-		Username: req.Username,
-		Email:    req.Email,
-		Password: string(cryptedPw),
-		IsAdmin:  req.Role == "admin",
+	return transaction.RunWithAutoCommit(&sess, func() error {
+		_, err := svc.repo.FindUserByUsername(ctx, req.Username)
+
+		if err == nil {
+			// duplicate username
+			return ErrUsernameAlreadyRegistered
+		}
+
+		if !errors.Is(err, auth.ErrUsernameNotFound) {
+			// unexpected kind of error
+			return errorutil.AddCurrentContext(err)
+		}
+
+		_, err = svc.repo.FindUserByEmailAndIsAdmin(ctx, req.Email, req.Role == "admin")
+
+		if err == nil {
+			return ErrEmailAlreadyRegistered
+		}
+
+		if !errors.Is(err, auth.ErrEmailAndIsAdminNotFound) {
+			// unexpected kind of error
+			return errorutil.AddCurrentContext(err)
+		}
+
+		cryptedPw, err := bcrypt.GenerateFromPassword(
+			[]byte(req.Password),
+			svc.bcryptCost,
+		)
+		if err != nil {
+			return errorutil.AddCurrentContext(err)
+		}
+
+		repoRes, err := svc.repo.CreateUser(ctx, auth.User{
+			Id:       uuid.New().String(),
+			Username: req.Username,
+			Email:    req.Email,
+			Password: string(cryptedPw),
+			IsAdmin:  req.Role == "admin",
+		})
+		if err != nil {
+			return errorutil.AddCurrentContext(err)
+		}
+
+		token, err := svc.generateToken(repoRes)
+		if err != nil {
+			return errorutil.AddCurrentContext(err)
+		}
+
+		*res = RegisterUserRes{
+			AccessToken: token,
+		}
+		return nil
 	})
-	if err != nil {
-		return errorutil.AddCurrentContext(err)
-	}
-
-	token, err := svc.generateToken(repoRes)
-	if err != nil {
-		return errorutil.AddCurrentContext(err)
-	}
-
-	*res = RegisterUserRes{
-		AccessToken: token,
-	}
-
-	return nil
 }
