b5f0d7c4c232de7c3c8e6da3faa75c71c5cfc540
Jesse Nicholas <jessenicholas.t@gmail.com>
2024-05-25 14:53:17 +0700

    refactor merchant items by filter repository later

--- repos/merchant/find_items_by_filter.go
+++ repos/merchant/find_items_by_filter.go
@@ -5,105 +5,101 @@ import (
 
 	"github.com/JesseNicholas00/BeliMang/utils/errorutil"
 	"github.com/JesseNicholas00/BeliMang/utils/mewsql"
+	"github.com/JesseNicholas00/BeliMang/utils/transaction"
 )
 
 // AdminListMerchant implements MerchantRepository.
-func (lol *merchantRepoImpl) FindMerchantItemsByFilter(ctx context.Context,
+func (repo *merchantRepoImpl) FindMerchantItemsByFilter(ctx context.Context,
 	filter MerchantItemListFilter,
 ) (res []MerchantItem, total int64, err error) {
 	if err = ctx.Err(); err != nil {
 		return
 	}
 
-	var conditions []mewsql.Condition
-
-	if filter.MerchantItemId != nil {
-		conditions = append(
-			conditions,
-			mewsql.WithCondition("merchant_item_id = ?", *filter.MerchantItemId),
-		)
-	}
-
-	if filter.Name != nil {
-		conditions = append(
-			conditions,
-			mewsql.WithCondition("name ILIKE ?", "%"+*filter.Name+"%"),
-		)
-	}
-
-	if filter.Category != nil {
-		conditions = append(
-			conditions,
-			mewsql.WithCondition("category = ?", *filter.Category),
-		)
-	}
-
-	options := []mewsql.SelectOption{
-		mewsql.WithWhere(conditions...),
-	}
-
-	//get count before adding pagination
-	sqlCount, args := mewsql.Select(
-		"count(*) as total",
-		"merchant_items",
-		options...,
-	)
-
-	ctx, sess, err := lol.dbRizz.GetOrNoTx(ctx)
+	ctx, sess, err := repo.dbRizz.GetOrAppendTx(ctx)
 	if err != nil {
 		err = errorutil.AddCurrentContext(err)
 		return
 	}
 
-	countRows, err := sess.Ext.QueryxContext(ctx, sqlCount, args...)
-	if err != nil {
-		err = errorutil.AddCurrentContext(err)
-		return
-	}
-	defer countRows.Close()
+	err = transaction.RunWithAutoCommit(&sess, func() error {
+		var conditions []mewsql.Condition
 
-	for countRows.Next() {
-		var cur Total
-		if err = countRows.StructScan(&cur); err != nil {
-			err = errorutil.AddCurrentContext(err)
-			return
+		if filter.MerchantItemId != nil {
+			conditions = append(
+				conditions,
+				mewsql.WithCondition("merchant_item_id = ?", *filter.MerchantItemId),
+			)
+		}
+		if filter.Name != nil {
+			conditions = append(
+				conditions,
+				mewsql.WithCondition("name ILIKE ?", "%"+*filter.Name+"%"),
+			)
+		}
+		if filter.Category != nil {
+			conditions = append(
+				conditions,
+				mewsql.WithCondition("category = ?", *filter.Category),
+			)
+		}
+		options := []mewsql.SelectOption{
+			mewsql.WithWhere(conditions...),
 		}
 
-		total = cur.Total
-	}
-
-	options = append(options, mewsql.WithLimit(filter.Limit),
-		mewsql.WithOffset(filter.Offset))
-
-	if filter.CreatedAtSort != nil {
-		options = append(
-			options,
-			mewsql.WithOrderBy("created_at", *filter.CreatedAtSort),
+		//get count before adding pagination
+		sqlCount, args := mewsql.Select(
+			"count(*) as total",
+			"merchant_items",
+			options...,
 		)
-	}
 
-	sql, args := mewsql.Select(
-		"merchant_item_id, name, category, price, image_url, created_at",
-		"merchant_items",
-		options...,
-	)
+		countRows, err := sess.Ext.QueryxContext(ctx, sqlCount, args...)
+		if err != nil {
+			return errorutil.AddCurrentContext(err)
+		}
+		defer countRows.Close()
 
-	rows, err := sess.Ext.QueryxContext(ctx, sql, args...)
-	if err != nil {
-		err = errorutil.AddCurrentContext(err)
-		return
-	}
-	defer rows.Close()
+		for countRows.Next() {
+			var cur Total
+			if err = countRows.StructScan(&cur); err != nil {
+				return errorutil.AddCurrentContext(err)
+			}
 
-	for rows.Next() {
-		var cur MerchantItem
-		if err = rows.StructScan(&cur); err != nil {
-			err = errorutil.AddCurrentContext(err)
-			return
+			total = cur.Total
 		}
 
-		res = append(res, cur)
-	}
+		options = append(options, mewsql.WithLimit(filter.Limit),
+			mewsql.WithOffset(filter.Offset))
 
+		if filter.CreatedAtSort != nil {
+			options = append(
+				options,
+				mewsql.WithOrderBy("created_at", *filter.CreatedAtSort),
+			)
+		}
+
+		sql, args := mewsql.Select(
+			"merchant_item_id, name, category, price, image_url, created_at",
+			"merchant_items",
+			options...,
+		)
+
+		rows, err := sess.Ext.QueryxContext(ctx, sql, args...)
+		if err != nil {
+			return errorutil.AddCurrentContext(err)
+		}
+		defer rows.Close()
+
+		for rows.Next() {
+			var cur MerchantItem
+			if err = rows.StructScan(&cur); err != nil {
+				return errorutil.AddCurrentContext(err)
+			}
+
+			res = append(res, cur)
+		}
+		return nil
+	})
 	return
 }
