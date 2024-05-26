37a6a3607772c707166da3a28c43847bd3db901b
Jesse Nicholas <jessenicholas.t@gmail.com>
2024-05-26 15:59:36 +0700

    fix data migration

--- migrations/20240526081758_add_merchant_item_table_indexes.up.sql
+++ migrations/20240526081758_add_merchant_item_table_indexes.up.sql
@@ -1,2 +1,2 @@
 CREATE INDEX merchant_item_category ON merchant_items(category);
-CREATE INDEX merchant_created_at ON merchant(created_at);
+CREATE INDEX merchant_item_created_at ON merchant_items(created_at);
